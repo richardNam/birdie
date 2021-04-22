f3f2ec461b3eab2a4d61feae7f2c299fd7cf0131
Richard <richardhnam@gmail.com>
2021-04-22 13:44:17 -0700

    update

--- birdie/db/sql_client.py
+++ birdie/db/sql_client.py
@@ -3,13 +3,6 @@ import pandas as pd
 from . import db_config
 
 
-TABLE_NAME_Q = '''
-SELECT TABLE_NAME AS table_names
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='{db}'
-'''
-
-
 class SQLClient:
     def __init__(self, engine):
         self.engine = engine
@@ -17,10 +10,10 @@ class SQLClient:
 
     def _get_table_names(self):
         tables_df = pd.read_sql_query(
-            TABLE_NAME_Q.format(db=db_config.db_name),
+            'show tables',
             con=self.engine
         )
-        return tables_df.table_names.tolist()
+        return tables_df.Tables_in_stocks.tolist()
 
 
     def replace_table(self, df, table_name):
@@ -33,7 +26,8 @@ class SQLClient:
         """
         table_names = self._get_table_names()
         if table_name not in table_names:
-            raise Exception('Table name does not exist in database.')
+            raise Exception('Table {} does not exist in database.'.format(
+                table_name))
 
         df.to_sql(
             table_name,
