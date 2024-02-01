49b966628ac67aa358a12d8d9e63b7fbfa53799d
Richard Nam <richardnam@gmail.com>
2024-02-01 00:01:44 -0800

    bump

--- etl.py
+++ etl.py
@@ -2,24 +2,22 @@ import config
 
 from birdie.db.util import get_engine
 from birdie.db.sql_client import SQLClient
-from birdie.etl.vantage.handler import VantageHandler
-from birdie.etl.vantage.client import AlphaVantageClient
+from birdie.etl.bat.handler import VantageHandler
+from birdie.etl.bat.client import AlphaVantageClient
 
 
 if __name__ == '__main__':
-    # Alpha Vantage Client
-    stocks_engine = get_engine('stocks')
-    vantage_handler = VantageHandler(
-        sql_client=SQLClient(engine=stocks_engine),
-        data_client=AlphaVantageClient(
-            key=config.alpha_vantage_key
-        ),
-        symbols=config.symbols,
-        sleep_seconds=config.sleep_seconds,
-    )
-    vantage_handler.run()
 
-
-
+    # BaT Client
+    # Add BaT job here. stocks_engine = get_engine('stocks')
+    # Add BaT job here. vantage_handler = VantageHandler(
+    # Add BaT job here.     sql_client=SQLClient(engine=stocks_engine),
+    # Add BaT job here.     data_client=AlphaVantageClient(
+    # Add BaT job here.         key=config.alpha_vantage_key
+    # Add BaT job here.     ),
+    # Add BaT job here.     symbols=config.symbols,
+    # Add BaT job here.     sleep_seconds=config.sleep_seconds,
+    # Add BaT job here. )
+    # Add BaT job here. vantage_handler.run()
 
 
